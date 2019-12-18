5719ceb0648773013a06f543e61b246900528b5a
YMyajima <37886440+YMyajima@users.noreply.github.com>
2019-12-18 13:47:26 +0900

    Revert "Develop"

--- src/main/java/com/sample/paypal/utils/DateFormatUtils.java
+++ src/main/java/com/sample/paypal/utils/DateFormatUtils.java
@@ -10,10 +10,4 @@ public class DateFormatUtils {
         String strDateTime = dateTime.format(dateTimeFormatter);
         return strDateTime;
     }
-    
-    public static String toStrVersion2(ZonedDateTime dateTime, final String format) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
-        String strDateTime = dateTime.format(dateTimeFormatter);
-        return strDateTime;
-    }
 }
